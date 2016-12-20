4c40e940b2627ad9bde400cdc0a311de6608aa8c
dbhaig <time4tux@gmail.com>
2016-12-20 16:44:44 -0500

    Commented out line in change_channel.sh

--- scripts/change_channel.sh
+++ scripts/change_channel.sh
@@ -6,7 +6,7 @@
 #  MIT Licence (See LICENSE.txt for details)
 #
 # Creation Date: 21-11-2015
-LAST_MODIFIED="Sat 03 Dec 2016 12:12:31 AM EST -0500 "
+LAST_MODIFIED="Tue 20 Dec 2016 04:42:44 PM EST -0500 "
 VERSION=0.1	
 #
 # Purpose: A script for MythTV to call to change the channel on 
@@ -18,7 +18,7 @@ LOG_FILE=/home/mythtv/change_channel.log
 date=$(date +%Y-%m-%d-%H:%M:%S)
 me=$(whoami)
 if $(/home/mythtv/talk2irtx.py "CHANNEL $1" >> $LOG_FILE 2>&1); then
-   echo $result >> $LOG_FILE
+   # echo $result >> $LOG_FILE
    echo "$date 4642HD changed to channel'$1' $me" >> $LOG_FILE
    if $(/home/mythtv/pvr2_record.py $1 >> $LOG_FILE 2>&1 &); then
        echo "$date PVR2 record started" >> $LOG_FILE
